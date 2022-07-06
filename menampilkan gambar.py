4d068f1ac638f7c7c510f0a5a8c6d511ebe6bb93
Ahmadahsana <ahmadahsanataqwim@gmail.com>
2022-07-07 06:11:17 +0700

    percobaan yang gagal pada file .py

---
+++ menampilkan gambar.py
@@ -0,0 +1,15 @@
+import cv2
+import matplotlib.pyplot as plt
+
+gambar = cv2.imread('gambar/2.jpg')
+
+# plt.imshow(gambar)
+
+
+# while True:
+#     cv2.imshow('Hai', gambar)
+
+#     if cv2.waitKey(1) & 0xFF == 27:
+#         break
+
+# cv2.destroyAllWindows()
